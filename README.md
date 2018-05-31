34444dde4bc37f97c5e6fda87798770b6bd25ea1
Gimli2 <gimli2@gmail.com>
2018-05-31 15:53:33 +0200

    YANG models + DHCP hack.

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ You'll need the ``sysrepo-plugind`` in order to make this work.
 ### Installation
 
 Build and install ``sysrepo`` and then use a standard CMake setup for this.
+
+When loading and installing modules to sysrepo, e.g.:
+
+ sysrepoctl --install --yang ietf-ipv6-unicast-routing\@2018-03-21.yang
+
+pay attention to proper version of required modules, especially ietf-interfaces is working only with revision 2017-12-16. If you have another revision in your datastore you should uninstall it.
--- src/interfaces-plugin.cpp
+++ src/interfaces-plugin.cpp
@@ -138,6 +138,12 @@ static void create_interface(sr_session_ctx_t *session, char *name) {
     sr_val_t *type = get_val(session, "/ietf-interfaces:interfaces/interface[name='"+(string)name+"']/type");
     //printf("Type %d %d = %s\n", type->type, SR_IDENTITYREF_T, type->data.identityref_val);
     
+    sr_val_t *description = get_val(session, "/ietf-interfaces:interfaces/interface[name='"+(string)name+"']/description");
+    string sdesc = "; " + (string) description->data.string_val;
+    
+    // ugly temporary hack to determine DHCP - DHCP is used when description starts with "DHCP"
+    bool is_dhcp = (sdesc.substr(2,4).compare("DHCP") == 0);
+    
     // proceed only to enabled and known interface type
     if (enabled->data.bool_val && strcmp("iana-if-type:ethernetCsmacd", type->data.identityref_val) == 0) {
         // shortcut for xpath queries
@@ -145,6 +151,7 @@ static void create_interface(sr_session_ctx_t *session, char *name) {
         
         // prepare dict for output config
         ini_table_s* ifcfg = ini_table_create();
+        ini_table_create_entry(ifcfg, "Match", &sdesc[0u], "");
         ini_table_create_entry(ifcfg, "Match", "Name", name);
         
         sr_val_t *ipv4enabled = get_val(session, ifipv4xpath + "/enabled");
@@ -163,44 +170,37 @@ static void create_interface(sr_session_ctx_t *session, char *name) {
             string mtu = to_string(ipv4mtu->data.uint16_val);
             if (ipv4mtu != NULL) ini_table_create_entry(ifcfg, "Link", "MTUBytes", &mtu[0u]);
             
-            sr_val_t *values = NULL;
-            size_t count = 0;
-            int rc = SR_ERR_OK;
-            xpath = ifipv4xpath + "/address/ip";
-            rc = sr_get_items(session, &xpath[0u], &values, &count);
-            if (handle_sr_return(rc) == OK) {        
-                for (size_t i = 0; i < count; i++){
-                    //printf("IPv4 ip = %s\n", (&values[i])->data.string_val);
-                   
-                    sr_val_t *ipv4prefixlen = get_val(session, ifipv4xpath + "/address[ip='"+(string)(&values[i])->data.string_val+"']/prefix-length");
-                    //printf("IPv4 prefixlen = %d\n", ipv4prefixlen->data.uint8_val);
-                    
-                    // TODO: also netmask keyword is possible
-                    
-                    // possible origins: other, static, dhcp, link-layer (stateless IPv6), random -- see rfc7277
-                    sr_val_t *ipv4origin = get_val(session, ifipv4xpath + "/address[ip='"+(string)(&values[i])->data.string_val+"']/origin");
-                    if (ipv4origin != NULL) printf("IPv4 origin = %s\n", ipv4origin->data.string_val);
-                    
-                     if (ipv4origin == NULL || strcmp(ipv4origin->data.string_val, "static") == 0) {
-                        // static is default
+            if (is_dhcp) {
+                // DHCP
+                ini_table_create_entry(ifcfg, "Network", "DHCP", "ipv4");
+            } else {
+                // STATIC adresses            
+                sr_val_t *values = NULL;
+                size_t count = 0;
+                int rc = SR_ERR_OK;
+                xpath = ifipv4xpath + "/address/ip";
+                rc = sr_get_items(session, &xpath[0u], &values, &count);
+                if (handle_sr_return(rc) == OK) {        
+                    for (size_t i = 0; i < count; i++){
+                        //printf("IPv4 ip = %s\n", (&values[i])->data.string_val);
+                       
+                        sr_val_t *ipv4prefixlen = get_val(session, ifipv4xpath + "/address[ip='"+(string)(&values[i])->data.string_val+"']/prefix-length");
+                        //printf("IPv4 prefixlen = %d\n", ipv4prefixlen->data.uint8_val);
+                        
+                        // TODO: also netmask keyword is possible
+                        
                         string addr = (string)(&values[i])->data.string_val + "/" + to_string(ipv4prefixlen->data.uint8_val);
                         // it is possible to have more addres, need to create entry allowing duplicate key
                         ini_table_create_entry_duplicate(ifcfg, "Network", "Address", &addr[0u]);
-                    } else if (ipv4origin != NULL && strcmp(ipv4origin->data.string_val, "dhcp") == 0) {
-                        // DHCP
-                        ini_table_create_entry(ifcfg, "Network", "DHCP", "ipv4");
-                    } else {
-                        printf("Not implemented.");
+                        
+                        // TODO: Gateway DNS
+                        // ini_table_create_entry(ifcfg, "Network", "Gateway", "");
+                        // ini_table_create_entry(ifcfg, "Network", "DNS", "");
+                        
+                        sr_free_val(ipv4prefixlen);
                     }
-                    
-                    // TODO: Gateway DNS
-                    // ini_table_create_entry(ifcfg, "Network", "Gateway", "");
-                    // ini_table_create_entry(ifcfg, "Network", "DNS", "");
-                    
-                    sr_free_val(ipv4origin);
-                    sr_free_val(ipv4prefixlen);
+                    sr_free_values(values, count);
                 }
-                sr_free_values(values, count);
             }            
             
             // TODO: get also neighbor* -> ip, link-layer-address
@@ -231,6 +231,7 @@ static void create_interface(sr_session_ctx_t *session, char *name) {
     
     sr_free_val(enabled);
     sr_free_val(type);
+    sr_free_val(description);
 }
 
 static void apply_current_config(sr_session_ctx_t *session) {
